9a8664dd92fd8b074f595bfea3dca9ce0ca4c4fe
fnussbaum <ferdinand.nussbaum@inf.ethz.ch>
2026-07-19 18:01:52 +0200

    Update grammar (allow terminating commas in predicates, macros)

--- spthy-ts-mode.el
+++ spthy-ts-mode.el
@@ -39,7 +39,7 @@
    'treesit-language-source-alist
    '(spthy "https://github.com/fnussbaum/tamarin-prover"
            :source-dir "tree-sitter/tree-sitter-spthy/src"
-           :commit "36d47f75150a028b53ce3ae8022994dc092d1e86")
+           :commit "a01c26e2296e395341f28ea5b6b66ad2b98e2c34")
    t)
   (when (treesit-ready-p 'spthy)
     (add-to-list 'auto-mode-alist '("\\.spthy\\'" . spthy-ts-mode))))
